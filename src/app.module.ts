f79efb4986a6818e5c6b1fbaf0a3e293920882a6
JeanDeboutGat <jeandebout.gatari.etu@univ-lille.fr>
2023-03-05 12:37:12 +0100

    feat(package.json): add mongoose, validators and transformers to the project.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogsModule } from './blogs/blogs.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [BlogsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/theoverview'),
+    BlogsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      enableDebugMessages: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  logger.log(`Application running on port ${port}`);
 }
+
 bootstrap();
