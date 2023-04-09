9a0baafb6aa819db95b336f61e17c986667f589d
jean <jeandebout.gatari.etu@univ-lille.fr>
2023-04-09 16:52:42 +0200

    feat(hash): save user password hashed with bcrypt.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './user.dto';
 import { UsersService } from './users.service';
+import { User } from './user.model';
 
-@Controller('signup')
+@Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-  @Post()
+
+  @Post('signup')
   private addUser(@Body() createUserDto: CreateUserDto): void {
     this.usersService.createUser(createUserDto).catch((e) => {
       console.log('SignUp: Add user failed.', e);
     });
+  }
+
+  @Get()
+  private async getUsers(): Promise<User[]> {
+    return await this.usersService.getUsers();
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './user.dto';
 import { User } from './user.model';
-import { UsersRepository } from "./users.repository";
+import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly userRepository: UsersRepository) {}
 
   async createUser(createUser: CreateUserDto) {
-    // hash password
-    const user = new User(createUser.username, createUser.email, createUser.password);
+    const hashedPassword = await bcrypt.hash(createUser.password, 10);
+
+    const user = new User(createUser.username, createUser.email, hashedPassword);
+
     await this.userRepository.createUser(user);
+  }
+
+  async getUsers(): Promise<User[]> {
+    return await this.userRepository.findAll();
   }
 }
