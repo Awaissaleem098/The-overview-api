4f0a71f0bdb3fa4e3f7f3b2c09bc07e46b4f4f5c
JeanDeboutGat <jeandebout.gatari.etu@univ-lille.fr>
2023-08-16 20:59:00 +0200

    conf(config): add a configuration dependency + use mongo env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TutorialsModule } from './tutorials/tutorials.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import * as process from 'process';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/theoverview'), BlogsModule, TutorialsModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`mongodb://${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/theoverview`),
+    BlogsModule,
+    TutorialsModule,
+    UsersModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
